5a0ad7c9aef562b08265691f572f6d084b56c738
Subhadip Ghosh <subhadipghosh11@gmail.com>
2021-03-27 23:07:34 +0530

    updated

--- bin/backup.sh
+++ bin/backup.sh
@@ -22,6 +22,14 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+exec_admin_command() {
+    if $user_has_sudo; then
+        sudo $1
+    else
+        su -c "$1"
+    fi
+}
+
 # Find the pretty hostname
 # backup_laptop
 comp_name=`hostnamectl --pretty`
@@ -31,14 +39,23 @@ if [ -z $comp_name ]; then
     exit 1
 fi
 
+user_has_sudo=false
+sudo -v 2>/dev/null
+if [ $? -eq 0 ]; then
+    user_has_sudo=true
+fi
+#TODO remove
+echo User sudo status: $user_has_sudo
+
 # List of directories that needs to be backed up
 # Paths are relative from the user's home directory
 backed_dirs[0]='Documents'
-backed_dirs[1]='Notebooks'
-backed_dirs[2]='Music'
-backed_dirs[3]='Pictures'
-backed_dirs[4]='Videos'
+#backed_dirs[1]='Notebooks'
+#backed_dirs[2]='Music'
+#backed_dirs[3]='Pictures'
+#backed_dirs[4]='Videos'
 
+#TODO remove
 #declare -A synced_dirs
 #synced_dirs["/home/${USER}/Documents"]="backup_laptop"
 #synced_dirs["/home/${USER}/Notebooks"]="backup_laptop"
@@ -64,32 +81,35 @@ done <<<`lsblk -f -o NAME,LABEL -l -n -p`
 IFS=$IFS_ORIG
 
 if [ -z "$device_path" ]; then
-    echo -e "Device path not found. Exiting"
+    echo "Device path not found. Exiting"
 fi
 
-# mount the external drive
-echo "Mounting ${device_path} to ${mount_path}. Continue?"
+# Mount the external drive
+echo Mounting ${device_path} to ${mount_path}. Continue?
 read choice
-if test "$choice" != "y"
-then
-    echo "$choice"
-    echo Exiting.
+if [ "$choice" != "y" ]; then
+    echo Selected $choice. Exiting.
     exit 1
 fi
-if test ! -d ${mount_path}
-then
-    echo Path ${mount_path} does not exists. Creating.
-    sudo mkdir -p ${mount_path}
-fi
-sudo mount -t ntfs ${device_path} ${mount_path}
 
-# sync every path in synced_dir
-for sourceDir in "${!synced_dirs[@]}"
+# Create the mount path if not exists
+if [ ! -d ${mount_path} ]; then
+    echo Path ${mount_path} does not exists. Creating..
+    exec_admin_command "mkdir -p ${mount_path}"
+fi
+
+echo Mounting..
+exec_admin_command "mount -t ntfs ${device_path} ${mount_path}"
+
+echo
+
+# Sync every path in synced_dir
+for dir in "${backed_dirs[@]}"
 do
-    echo -e "\nSyncing ${sourceDir} to ${mount_path}/${synced_dirs[$sourceDir]}"
-    rsync -av --delete "${sourceDir}" "${mount_path}/${synced_dirs[$sourceDir]}"
+    echo Syncing /home/${USER}/${dir} to ${mount_path}/${comp_name}/${dir} ..
+    rsync -av --delete "/home/${USER}/${dir}" "${mount_path}/${comp_name}"
 done
 
-# unmount the external drive
-echo "Unmounting ${device_path}"
-sudo umount ${device_path}
+# Unmount the external drive
+echo Unmounting ${device_path} ..
+exec_admin_command "umount ${device_path}"
