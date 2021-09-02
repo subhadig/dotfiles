0540d7f0f6f0e22726b3a88bf2457bcf9642d3c0
Subhadip Ghosh <subhadipghosh11@gmail.com>
2021-09-02 13:25:52 +0530

    updated

--- tmux/work-tmux.sh
+++ tmux/work-tmux.sh
@@ -4,6 +4,6 @@ editor='nvim'
 
 tmux new-session -d -s "dev" -n "notes" -c ~/workspaces/personal/notes $editor
 tmux new-window -c ~/"OneDrive - Pegasystems Inc"/notes/ -n "pega-notes" $editor
-tmux new-window -c ~/workspaces/pega/prpc/prpc-platform -n "prpc-platform"
+tmux new-window -c ~/workspaces/pega/prpc -n "prpc-platform"
 tmux new-window -c ~/"OneDrive - Pegasystems Inc" -n "tmp-notes" $editor temp.notes
 tmux attach-session -d -c "~"
