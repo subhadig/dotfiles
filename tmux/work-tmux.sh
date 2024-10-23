7e28cff58b26500646dcc023fbf79bdbcebd778c
Subhadip Ghosh <subhadipghosh11@gmail.com>
2024-10-23 20:37:28 +0530

    updated

--- tmux/work-tmux.sh
+++ tmux/work-tmux.sh
@@ -5,6 +5,6 @@ editor='nvim'
 tmux new-session -d -s "dev" -n "notes" -c ~/workspaces/personal/notes $editor
 tmux new-window -c ~/"OneDrive - Pegasystems Inc"/notes/ -n "pega-notes" $editor
 tmux new-window -c ~/workspaces/pega -n "pega-workspace"
-tmux new-window -c ~/"OneDrive - Pegasystems Inc" -n "tmp-notes" $editor temp.notes
+tmux new-window -c ~/"OneDrive - Pegasystems Inc" -n "tmp-notes" $editor -p temp.notes todo.txt
 tmux select-window -t 2
 tmux attach-session -d -c "~"
