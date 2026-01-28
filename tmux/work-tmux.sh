78b8de08f8031724d51c389b9084a360f482724b
Subhadip Ghosh <subhadipghosh11@gmail.com>
2026-01-28 22:25:13 +0530

    updated

--- tmux/work-tmux.sh
+++ tmux/work-tmux.sh
@@ -8,8 +8,8 @@ tmux has-session -t "$session_name" 2>/dev/null
 if [ $? != 0 ]; then
     tmux new-session -d -s "$session_name" -n "notes" -c ~/workspaces/personal/notes $editor
     tmux new-window -c ~/"OneDrive - Pegasystems Inc"/notes/ -n "pega-notes" $editor
-    tmux new-window -c ~/workspaces/pega -n "pega-workspace"
-    tmux new-window -c ~/"OneDrive - Pegasystems Inc" -n "tmp-notes" $editor -p temp.notes todo.txt
+    tmux new-window -c ~/workspaces/pega/infinity -n "pega-workspace"
+    tmux new-window -c ~/"OneDrive - Pegasystems Inc" -n "tmp-notes" $editor -p temp.notes todo.md
     tmux select-window -t 2
 fi
 
